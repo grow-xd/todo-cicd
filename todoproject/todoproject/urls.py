490ed0810ce04c6e971b16d3188c5785a8e89f8e
grow-xd <gupta.rg.rishabh@gmail.com>
2024-06-28 00:01:12 +0530

    changes in settings.py and urls.py

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from todo.views import ToDoViewSet
-
+from django.conf import settings
+from  django.conf.urls.static import static
 router = DefaultRouter()
 router.register(r'todos', ToDoViewSet)
 
@@ -25,3 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
